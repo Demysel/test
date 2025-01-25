5f03c7d3fac1bc5c3da56b48968c7658c8b67e92
Demysel <84205147+Demysel@users.noreply.github.com>
2025-01-25 10:55:19 +0100

    Update firebase.js

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,21 +1,20 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from 'firebase/app';
+import { getFirestore, collection } from 'firebase/firestore';
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAU19mm6RDWy_fhoMo3SLWFuyvT4UDKKKk",
-  authDomain: "calendrier-300cb.firebaseapp.com",
-  projectId: "calendrier-300cb",
-  storageBucket: "calendrier-300cb.firebasestorage.app",
-  messagingSenderId: "556904721649",
-  appId: "1:556904721649:web:8d40a30990520e4ffbcd50",
-  measurementId: "G-9G8217QW9D"
+  apiKey: import.meta.env.AIzaSyAU19mm6RDWy_fhoMo3SLWFuyvT4UDKKKk,
+  authDomain: import.meta.env.calendrier-300cb.firebaseapp.com,
+  projectId: import.meta.env.calendrier-300cb,
+  storageBucket: import.meta.env.calendrier-300cb.firebaseapp.com,
+  messagingSenderId: import.meta.env.556904721649,
+  appId: import.meta.env.1:556904721649:web:8d40a30990520e4ffbcd50
 };
 
-// Initialize Firebase
+// Initialisation
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const db = getFirestore(app);
+
+// Création de la référence de collection
+const eventsCollection = collection(db, 'events');
+
+export { db, eventsCollection }; // Export nommé correct
