726b713550a8e72196b9beca90414181ff49592b
Demysel <84205147+Demysel@users.noreply.github.com>
2025-01-25 10:59:03 +0100

    Update firebase.js

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -5,7 +5,7 @@ const firebaseConfig = {
   apiKey: import.meta.env.AIzaSyAU19mm6RDWy_fhoMo3SLWFuyvT4UDKKKk,
   authDomain: import.meta.env.calendrier-300cb.firebaseapp.com,
   projectId: import.meta.env.calendrier-300cb,
-  storageBucket: import.meta.env.calendrier-300cb.firebaseapp.com,
+  storageBucket: import.meta.env.calendrier-300cb.firebasestorage.app
   messagingSenderId: import.meta.env.556904721649,
   appId: import.meta.env.1:556904721649:web:8d40a30990520e4ffbcd50
 };
